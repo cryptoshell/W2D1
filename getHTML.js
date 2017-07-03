e760711638f80a3fa623fb59f4c7c177f84ec71d
Yelim Oh <yelim.oh1@gmail.com>
2017-07-03 18:43:07 +0000

    Added callback function step 4

---
+++ getHTML.js
@@ -0,0 +1,24 @@
+const https = require("https");
+
+function getHTML (options, printHTML(html)) {
+
+  https.get(requestOptions, function (response) {
+  response.setEncoding('utf8');
+  response.on('data', function (data) {
+    var allData = '';
+    allData += data;
+    console.log('All Data Received:', allData);
+  });
+
+  response.on('end', printHTML(html));
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
