41cae7d6d7a24ca3b294f0b123badbafca0ec9b5
Yelim Oh <yelim.oh1@gmail.com>
2017-07-03 18:21:02 +0000

    Added second way to print data

---
+++ getAndPrintHTML.js
@@ -0,0 +1,22 @@
+const https = require("https");
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+  response.setEncoding('utf8');
+  response.on('data', function (data) {
+    var allData = '';
+    allData += data;
+    console.log('All Data Received:', allData);
+  });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+}
